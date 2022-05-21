d75a7c01d1c3cdb40609bf402ff89e284b75710c
Re_MoNu <78672384+Re-MoNu@users.noreply.github.com>
2022-05-21 12:56:08 +0900

    Added Summery and Favicon
    sus

--- script.js
+++ script.js
@@ -20,13 +20,15 @@
 
         // Convenience function to setup a webcam
         const flip = true; // whether to flip the webcam
-        webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+        webcam = new tmImage.Webcam(400, 400, flip); // width, height, flip
+        
         await webcam.setup(); // request access to the webcam
         await webcam.play();
+        document.getElementById("webcam-container").innerHTML="";
         window.requestAnimationFrame(loop);
-
         // append elements to the DOM
         document.getElementById("webcam-container").appendChild(webcam.canvas);
+        
         labelContainer = document.getElementById("label-container");
         for (let i = 0; i < maxPredictions; i++) { // and class labels
             labelContainer.appendChild(document.createElement("div"));
@@ -44,13 +46,33 @@
         // predict can take in an image, video or canvas html element
         const prediction = await model.predict(webcam.canvas);
         let tempCount=1;
+        let guitarCount=0;
+        let guitarMuch=0;
         for (let i = 0; i < maxPredictions; i++) {
             const classPrediction =
                 parseInt(prediction[i].probability.toFixed(2)*100);
             
             document.getElementById("bar-0" + tempCount).style.width = classPrediction + "%";
-            document.getElementById("bar-0" + tempCount).innerText = prediction[i].className + ": " + classPrediction + "%"
+            document.getElementById("bar-0" + tempCount).innerText = prediction[i].className + ": " + classPrediction + "%";
+            if(parseInt(prediction[i].probability.toFixed(2)*100) > guitarMuch+10){
+                guitarCount=i;
+                guitarMuch = parseInt(prediction[i].probability.toFixed(2)*100);
+            }
             tempCount++;
         }
+        switch(parseInt(guitarCount)) {
+            case 0:
+                document.getElementById("thinkbox").innerText = "TM: I think this is an Electric Guitar!"
+                break;
+            case 1:
+                document.getElementById("thinkbox").innerText = "TM: I think this is a Bass Guitar!"
+                break;
+            case 2:
+                document.getElementById("thinkbox").innerText = "TM: I think this is an Acoustic Guitar!"
+                break;
+            default:
+                document.getElementById("thinkbox").innerText = "Error"
+                break;
+        }
     }
-    
+//document.getElementById("thinkbox").innerText = "I think this is a(n) " + prediction[i].className + " Guitar!"
